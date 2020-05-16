166d906cecfa427f04adae1876eedb0981263c60
Yuvraj Patil <ypatil2@slb.com>
2020-05-16 12:39:43 +0530

    Init commit

---
+++ BMM-POC/src/app/parameters-list/parameter.ts
@@ -0,0 +1,130 @@
+export const Parameters = [
+  {
+    className: 'Adoption_rate',
+    parameterName: 'Adoption rate',
+    parameterWeight: 20,
+    parameterOptions: [
+      {
+        weight: 6.25,
+        name: 'Adoption number is stable.',
+      },
+      {
+        weight: 12.5,
+        name: 'Steady increase in adoption numbers.',
+      },
+      {
+        weight: 18.75,
+        name: 'Steady increase in adoption rates with active users.',
+      },
+      {
+        weight: 25,
+        name: 'Adoption numbers are stable with highest active users.',
+      },
+    ],
+  },
+  {
+    className: 'NPT_decrease',
+    parameterName: 'NPT decrease',
+    parameterWeight: 25,
+    parameterOptions: [
+      {
+        weight: 6.25,
+        name: 'NPT reduction is achieved for some individual small activities.',
+      },
+      {
+        weight: 12.25,
+        name:
+          'NPT reduction is achieved for partial activity workflow, significant activities and are yet to be onboarded on digitization strategy.',
+      },
+      {
+        weight: 18.75,
+        name:
+          'NPT reduction is achieved through process optimization, automation and waste elimination.',
+      },
+      {
+        weight: 25,
+        name:
+          'NPT reduction is achieved across major part of a business activity workflow through digitization.',
+      },
+    ],
+  },
+  {
+    className: 'Technology_Adoption',
+    parameterName: 'Technology Adoption',
+    parameterWeight: 20,
+    parameterOptions: [
+      {
+        weight: 5,
+        name:
+          'Focus is on resolving individual problems and deliver digitial solutions to best solution those.',
+      },
+      {
+        weight: 10,
+        name:
+          'Strategised digital products implementation to scale multiple functions and business with clear visibility with or without a combination of technology stack.',
+      },
+      {
+        weight: 15,
+        name:
+          'A solution based technology stack(combination of automation, chat agents, mobile and cloud solutions) is identfied and implemented with multiple launches for an end to end solution.',
+      },
+      {
+        weight: 20,
+        name: 'Technology is inbuilt by now, and is driving innovation.',
+      },
+    ],
+  },
+  {
+    className: 'ROI',
+    parameterName: 'ROI',
+    parameterWeight: 20,
+    parameterOptions: [
+      {
+        weight: 7.5,
+        name:
+          'Gain due to Reduction in operational cost with respect to investment in digital activities.',
+      },
+      {
+        weight: 15,
+        name:
+          'Steady rate of decrease in operational cost visible to deployement phases of digital solutions.',
+      },
+      {
+        weight: 22.5,
+        name:
+          'Exponential decrease in operational cost resulting in investing in new initiatives.',
+      },
+      {
+        weight: 30,
+        name:
+          'Significant ROI is gained through digitization and funding for innovation is made available.',
+      },
+    ],
+  },
+  {
+    className: 'Digital_Drivers',
+    parameterName: 'Digital Drivers',
+    parameterWeight: 15,
+    parameterOptions: [
+      {
+        weight: 3.75,
+        name: 'Sponsor approvals after digitization identified to a pain area.',
+      },
+      {
+        weight: 7.5,
+        name:
+          'Digitization planned as part of the workflow experience for the quarter / year.',
+      },
+      {
+        weight: 11.25,
+        name:
+          'Leadership strategically plans and budgets for digitalization and predict revenue / ROI from the digital initiaves.',
+      },
+      {
+        weight: 15,
+        name:
+          'Joint venture with digital partner included in strategic planning.',
+      },
+    ],
+  },
+];
---
+++ BMM-POC/src/app/parameters-list/parameters-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ParametersListComponent } from './parameters-list.component';
+
+describe('ParametersListComponent', () => {
+  let component: ParametersListComponent;
+  let fixture: ComponentFixture<ParametersListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ParametersListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ParametersListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
